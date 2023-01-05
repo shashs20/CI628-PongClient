a5a2a393561fb8672377ffcc2dd74effa93e2816
shashs20 <69312805+shashs20@users.noreply.github.com>
2023-01-05 20:19:41 +0000

    Update MyGame.cpp

--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -10,7 +10,7 @@ void MyGame::on_receive(std::string cmd, std::vector<std::string>& args) {
             game_data.ballY = stoi(args.at(3));
         }
     } else {
-        std::cout << "Received: " << cmd << std::endl;
+        std::cout << "Received: " << cmd << std::endl; 
     }
 }
 
@@ -23,14 +23,40 @@ void MyGame::input(SDL_Event& event) {
         case SDLK_w:
             send(event.type == SDL_KEYDOWN ? "W_DOWN" : "W_UP");
             break;
+        case SDLK_s:
+             send(event.type == SDL_KEYDOWN ? "S_DOWN" : "S_UP");
+            break;
+        case SDLK_k:
+            send(event.type == SDL_KEYDOWN ? "K_DOWN" : "K_UP");
+            break;
+        case SDLK_i:
+            send(event.type == SDL_KEYDOWN ? "I_DOWN" : "I_UP");
+            break;
     }
 }
 
-void MyGame::update() {
+void MyGame::update() { 
     player1.y = game_data.player1Y;
+    player2.y = game_data.player2Y;
 }
 
 void MyGame::render(SDL_Renderer* renderer) {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderDrawRect(renderer, &player1);
+    SDL_RenderDrawRect(renderer, &player2);
+    SDL_RenderFillRect(renderer, &ball);
+    SDL_RenderDrawRect(renderer, &ball);
+
+    // Draw the net
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+    for (int y = 0; y < 600; ++y)
+    {
+        if (y % 5)
+        {
+            SDL_RenderDrawPoint(renderer, 800 / 2, y);
+        }
+    }
+
+    SDL_RenderPresent(renderer);
 }
